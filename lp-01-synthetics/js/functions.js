b0a90e13459000af79f0af2d9000054c85ed05be
marlon-deriv <marlon@deriv.com>
2022-10-17 15:25:17 +0200

    Launched first version of lp-01

---
+++ lp-01-synthetics/js/functions.js
@@ -0,0 +1,37 @@
+// Open the default tab when the page loads:
+document.getElementById("defaultOpen").click();
+
+function openTab(evt, tabName) {
+    // Variables:
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tab-content" and hide them
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tab-links" and remove the class "active"
+    tablinks = document.getElementsByClassName("tab-links");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+  function openDescription(evt, descName) {
+    // Variables:
+    var i, description, assetsDescs;
+
+    // Get element with id selected and hide them
+    description = document.getElementById(descName);
+
+    if(description.style.display == "block"){
+        description.style.display = "none";
+    } else {
+        description.style.display = "block";
+    }
+  }
